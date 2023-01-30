38edca1a43796e00750352a5f7c72cba31540403
allieamp <aampel1@binghamton.edu>
2023-01-30 17:20:54 -0500

    1/30 exercise

---
+++ ch01/exercises/data.py
@@ -0,0 +1,11 @@
+print (10*5)
+print(10**2)
+print(15/10)
+print(15//10)
+print(-15//10)
+print(15%10)
+print(10%15)
+print(10%10)
+print(0%10)
+print(10 / 15)
+##LIne 10 is wrong becasue it cuts off 0.66 prepeating but it cuts it off becasue its not worth the memory##
