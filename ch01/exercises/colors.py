5d03a1c2375cd0202479c4abf702f3a5fef74cca
allieamp <aampel1@binghamton.edu>
2023-01-30 18:03:33 -0500

    color exercise

--- ch01/exercises/colors.py
+++ ch01/exercises/colors.py
@@ -1,3 +1,9 @@
 colorlist = ["pink", "red", "purple", "blue", "green", "yellow", "orange"]
 print(len(colorlist))
-print(colorlist)
+print(colorlist[0])
+print(colorlist[1])
+print(colorlist[2])
+print(colorlist[3])
+print(colorlist[4])
+print(colorlist[5])
+print(colorlist[6])
