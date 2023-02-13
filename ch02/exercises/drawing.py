eeecdb3e47d2ae679833cb6a213e0872f6c49ba3
allieamp <aampel1@binghamton.edu>
2023-02-13 16:55:13 -0500

    2/13 turtle drawing

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -1,9 +1,9 @@
 import turtle
-sides = int(input("Enter # of sides"))
-length = float(input("Enter side length"))
+sides = int(input("Enter # of sides: "))
+length = int(input("Enter side length: "))
 franklin = turtle.Turtle()
 franklin.shape("turtle")
-franklin.color("red")
+franklin.color("pink")
 for _ in range(sides):
     franklin.forward(length)
     franklin.right(360/sides)
