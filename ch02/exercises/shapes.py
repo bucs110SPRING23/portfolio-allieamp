6726861fc42199507fa0e8d5982feccde31634d4
allieamp <aampel1@binghamton.edu>
2023-02-15 16:42:13 -0500

    2/15 snowman

--- ch02/exercises/shapes.py
+++ ch02/exercises/shapes.py
@@ -10,14 +10,14 @@ pygame.draw.circle(screen, "white", [650,200], 50)
 pygame.display.flip()
 pygame.time.wait(1000)
 
-for i in range(100, 1500, 100):
+for i in range(0, 900, 100):
     pygame.draw.circle(screen, "white", [100,i], 15)
+#pygame.display.flip()
+#pygame.time.wait(1000)
+    for i in range(0, 900, 100):
+        pygame.draw.circle(screen, "blue", [100,i], 15)
+        pygame.draw.circle(screen, "white", [100,i-25], 15)
+        pygame.display.flip()
+        pygame.time.wait(50)
 pygame.display.flip()
-pygame.time.wait(1000)
-for i in range(100, 1000, 100):
-    pygame.draw.circle(screen, "blue", [100,i], 15)
-    pygame.draw.circle(screen, "white", [100,i+25], 15)
-    pygame.display.flip()
-    pygame.time.wait(500)
-pygame.display.flip()
-pygame.time.wait(1000)
+pygame.time.wait(500)
