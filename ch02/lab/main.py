9cda4f550dc276e8055bbb5e6cf7023070d3593c
allieamp <aampel1@binghamton.edu>
2023-02-20 13:00:04 -0500

    2/20 lab

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -11,8 +11,8 @@ jack.shape("turtle")
 tina.shape("turtle")
 jack.color("blue")
 tina.color("purple")
-jack.speed(2)
-tina.speed(2)
+jack.speed(1)
+tina.speed(1)
 jack.up()
 tina.up()
 jack.setpos(-100,20)
@@ -32,7 +32,7 @@ jack.setpos(-100,20)
 tina.setpos(-100,-20)
 jack.down()
 tina.down()
-for i in range(1,5,1):
+for i in range(1,10,1):
     j = random.randrange(1,10)
     t = random.randrange(1,10)
     jack.forward(j)
@@ -41,26 +41,26 @@ jack.setpos(-100,20)
 tina.setpos(-100,-20)
 window.exitonclick()
 
+num_sides = int(3)
 window = pygame.display.set_mode()
-points = []
-num_side = 3
 side_length = int(50)
 xpos = int(100)
 ypos = int(100)
-window.fill("white")
-#for s in [3,4,6,20,100,360]:
-for i in range(0,num_side,1):
-    angle = 360/num_side
-    radians = math.radians(angle * 1)
-    x = xpos + side_length * math.cos(radians)
-    y = ypos + side_length * math.sin(radians)
-    points.append([x,y])
-    pygame.display.flip()
-pygame.display.flip()
-pygame.time.wait(500)
-pygame.draw.polygon(window, "purple", points, 5)
-pygame.display.flip()
-pygame.time.wait(500)
 window.fill("pink")
+
 pygame.display.flip()
-points = []
+for num_sides in [3,4,6,20,100,360]:
+    points = []
+    print(num_sides)
+    for i in range(0,num_sides,1):
+        angle = 360/num_sides
+        radians = math.radians(angle * i)
+        x = xpos + side_length * math.cos(radians)
+        y = ypos + side_length * math.sin(radians)
+        points.append([x,y])
+    print(num_sides)
+    pygame.draw.polygon(window, "black", points)
+    pygame.display.flip()
+    pygame.time.wait(5000)
+    window.fill("pink")
+    pygame.display.flip()
