b38bddf3df68696ca3a9b53ec56cbee8d6f77ef0
allieamp <aampel1@binghamton.edu>
2023-03-02 13:42:52 -0500

    3/2 lab

--- ch04/exercises/simon.py
+++ ch04/exercises/simon.py
@@ -3,41 +3,75 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode()
-width, height = screen.get_window_size()
+(width, height) = pygame.display.get_window_size() #returns tupule with width and height
 
+##creating hitboxes##
+
+#getting center screen
 hitbox_width = width / 2
 hitbox_height = height / 2
 
+#Rectangles keep track of size and position on screen
+#setting up the objects at the same place in the same size
 hitboxes = {
     "red": pygame.Rect(0,0,hitbox_width,hitbox_height),
     "green": pygame.Rect(0,0,hitbox_width,hitbox_height),
     "blue": pygame.Rect(0,0,hitbox_width,hitbox_height),
-    "Purple": pygame.Rect(0,0,hitbox_width,hitbox_height),
+    "purple": pygame.Rect(0,0,hitbox_width,hitbox_height),
 }
 
+#positioning hitboxes
 hitboxes["blue"].topleft = hitboxes ["red"].topright
 hitboxes["green"].topright = hitboxes ["red"].bottomright
 hitboxes["purple"].topleft = hitboxes ["red"].bottomright
 
-font = pygame.font.SysFont(None,24)
+#Defining hitbox colors
+main_colors = {
+    "red": (200,0,0),
+    "green":(0,200,0),
+    "blue":(0,0,200),
+    "purple":(200,0,200),
+}
+#Defining highlight colors
+highlight_colors = {
+    "red": (255,0,0),
+    "green":(0,255,0),
+    "blue":(0,0,255),
+    "purple":(255,0,255),
+}
 
+#create font object - None goes to the default font
+font = pygame.font.Font(None,48)
+#additional state data we'll need
 done = False
 result = []
 turns = 0
 order = list(hitboxes.keys())
-random.suffle(order)
 
-while not done:
-    for event in pygame.event.get():
+#top dwon programming - outlining - use keyword pass to count as no operation
+#Mainloop - frame rate
+#Each time through is one frame
+while not done: #basically "while true"
+    # 1. respond to events
+    for event in pygame.event.get(): #generic event loop to get all events since the last call
         if event.type == pygame.QUIT: #if they press the X
-            done = True
+            done = True #naturally end the program
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE: #if the press space bar reset
+            if event.key == pygame.K_SPACE: #if the press space bar it'll reset
                 random.suffle(order)
                 turns = len(order)
+                results = []
+                #display the sequence
+                for color in order: #for each color
+                    for c, hb in hitboxes.items(): #items means you want key and value - assigning c, hb to them
+                        pygame.draw.rect(screen,main_colors[c],hb) #draw them all on screen
+                    #changes box color to highlight color per color of loop and use rectangle object for spacing
+                    pygame.draw.rect(screen,highlight_colors[color],hitboxes[color])
+                    pygame.display.flip()
+                    pygame.time.delay(1000)
         elif event.type == pygame.MOUSEBUTTONDOWN:
             turns = turns - 1 #can only guess by the number of colors
-            if hitboxes["red"].collidepoint(event.pos):
+            if hitboxes["red"].collidepoint(event.pos): #using Rectanlges means we can just say "if its in the Rectangle"
                 result.append("red")
             elif hitboxes["green"].collidepoint(event.pos):
                 result.append("green")
@@ -45,15 +79,36 @@ while not done:
                 result.append("blue")
             elif hitboxes["purple"].collidepoint(event.pos):
                 result.append("purple")
-    if turns == 0:
-        if result == order:
-            font.render("You win!", True, "white")
-        else:
-            font.render("You loose", True, "white")
-    screen.fill((0,0,0))
+    #We've covered every event we care about
+    # 2. updating data
+    # if result and not turns: 
+    #     msg = [
+    #         "You entered:" + str(result),
+    #         "The correct pattern was:" + str(order)
+    #     ]
+    # if result == order:
+    #     msg.append("You won!")
+    # else:
+    #     msg.append("You lost")
+
+     #3. draw   
+    screen.fill("black")
     for c, hb in hitboxes.items():
-        box = pygame.draw.rect(screen, c, hb)
-        screen.blit(box, hb)
+        pygame.draw.rect(screen,main_colors[c], hb)
+
+    if result:
+        #result[1] means last color selected
+        pygame.draw.rect(screen,highlight_colors[result[-1]],hitboxes[result[-1]]) #color highlights when you click it
+    
+    #creates an image for the text for each message
+    # ypos = 60
+    # for m in msg:
+    #     text = font.render(m, True, "white") #creates the text
+    #     screen.blit(text,(20,ypos)) #puts it on screen at that position
+    #     ypos += 60 #acts an indent
+
+pygame.display.flip()
+
 
 
 
--- ch04/lab/main.py
+++ ch04/lab/main.py
@@ -2,33 +2,86 @@ import random
 import math
 import pygame
 pygame.init()
-screen = pygame.display.set_mode((500,500))
+screen = pygame.display.set_mode()
+(width, height) = pygame.display.get_window_size()
 
-screen.fill("blue")
+hitbox_width = int(width / 2)
+hitbox_height = int(height / 2)
+
+screen.fill("black")
+hitboxes = {
+    "Player1": pygame.Rect(0,100,hitbox_width/2,hitbox_height/2),
+    "Player2": pygame.Rect(50+hitbox_width/2,100,hitbox_width/2,hitbox_height/2),
+}
+box_colors = {
+    "Player1":(0,100,0),
+    "Player2":(100,0,100),
+}
+
+highlight_colors = {
+    "Player1":(0,200,0),
+    "Player2":(200,0,200),
+}
+
+font = pygame.font.Font(None, 48)
+text = font.render("Choose Who Will Win", True, "white")
+screen.blit(text, (100,50))
+pygame.draw.rect(screen,box_colors["Player1"],hitboxes["Player1"])
+pygame.draw.rect(screen,box_colors["Player2"],hitboxes["Player2"])
 pygame.display.flip()
-pygame.draw.circle(screen,"lightblue",(250,250),250)            
+
+bet = []
+done = False
+
+while not done: 
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if hitboxes["Player1"].collidepoint(event.pos):
+                bet.append("Player1")
+                pygame.draw.rect(screen,highlight_colors["Player1"],hitboxes["Player1"])
+                pygame.display.flip()
+                pygame.time.delay(500)
+                done = True
+            elif hitboxes["Player2"].collidepoint(event.pos):
+                bet.append("Player2")
+                pygame.draw.rect(screen,highlight_colors["Player2"],hitboxes["Player2"])
+                pygame.display.flip()
+                pygame.time.delay(500)
+                done = True
+
+
+screen.fill("black")
+pygame.display.flip()
+pygame.draw.circle(screen,"white",(hitbox_width,hitbox_height),hitbox_height+5)  
+pygame.draw.circle(screen,"blue",(hitbox_width,hitbox_height),hitbox_height)
+pygame.draw.circle(screen,"lightblue",(hitbox_width,hitbox_height),hitbox_height-50)
+pygame.draw.circle(screen,"blue",(hitbox_width,hitbox_height),hitbox_height-100)
+pygame.draw.circle(screen,"lightblue",(hitbox_width,hitbox_height),hitbox_height-150)
+pygame.draw.circle(screen,"blue",(hitbox_width,hitbox_height),hitbox_height-200)
+pygame.draw.circle(screen,"lightblue",(hitbox_width,hitbox_height),hitbox_height-250)
+pygame.draw.circle(screen,"blue",(hitbox_width,hitbox_height),hitbox_height-300)                        
 pygame.display.flip()            
-pygame.draw.line(screen,"black",(250,0),(250,500))
-pygame.draw.line(screen,"black",(0,250),(500,250))
+pygame.draw.line(screen,"black",(hitbox_width,0),(hitbox_width,height))
+pygame.draw.line(screen,"black",(0,hitbox_height),(width,hitbox_height))
 pygame.display.flip()
 
 player1_score = int(0)
 player2_score = int(0)
 for _ in range(10):
-    x_dart = random.randrange(0,500)
-    y_dart = random.randrange(0,500)
-    distance_from_center = math.hypot(250-x_dart, 250-y_dart)
-    is_in_circle = distance_from_center <= 500/2
+    x_dart = random.randrange(0,width)
+    y_dart = random.randrange(0,height)
+    distance_from_center = math.hypot(hitbox_width-x_dart, hitbox_height-y_dart)
+    is_in_circle = distance_from_center <= hitbox_width/2
     if is_in_circle == True:
         pygame.draw.circle(screen,"green",(x_dart,y_dart),5)
         player1_score = player1_score + 1
     elif is_in_circle == False:
         pygame.draw.circle(screen,"red",(x_dart,y_dart),5)
     pygame.display.flip()
-    x_dart = random.randrange(0,500)
-    y_dart = random.randrange(0,500)
-    distance_from_center = math.hypot(250-x_dart, 250-y_dart)
-    is_in_circle = distance_from_center <= 500/2
+    x_dart = random.randrange(0,width)
+    y_dart = random.randrange(0,height)
+    distance_from_center = math.hypot(hitbox_width-x_dart, hitbox_height-y_dart)
+    is_in_circle = distance_from_center <= hitbox_width/2
     if is_in_circle == True:
         pygame.draw.circle(screen,"purple",(x_dart,y_dart),5)
         player2_score = player2_score + 1
@@ -38,18 +91,27 @@ for _ in range(10):
     pygame.time.wait(500)
 
 
-print(player1_score, player2_score)
 if player1_score > player2_score:
+    winner = "Player 1"
     font = pygame.font.Font(None, 48)
     text = font.render("Player 1 Wins!", True, "white")
-    screen.blit(text, (250, 250))
+    screen.blit(text, (hitbox_width, hitbox_height))
 elif player1_score < player2_score:
+    winner = "Player 2"
     font = pygame.font.Font(None, 48)
     text = font.render("Player 2 Wins!", True, "white")
-    screen.blit(text, (250, 250))
+    screen.blit(text, (hitbox_width, hitbox_height))
 else:
     font = pygame.font.Font(None, 48)
     text = font.render("Tie!", True, "white")
-    screen.blit(text, (250, 250))
+    screen.blit(text, (hitbox_width, hitbox_height))
+if bet == winner:
+    font = pygame.font.Font(None, 48)
+    text = font.render("You Guessed Right", True, "white")
+    screen.blit(text, (hitbox_width, hitbox_height+100))
+else:
+    font = pygame.font.Font(None, 48)
+    text = font.render("You Guessed Wrong", True, "white")
+    screen.blit(text, (hitbox_width, hitbox_height+100))
 pygame.display.flip()
 pygame.time.wait(1000)
