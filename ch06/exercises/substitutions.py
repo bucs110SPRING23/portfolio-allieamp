4456328b326d7ea9120f6585b2f04de16ff405f8
allieamp <aampel1@binghamton.edu>
2023-03-20 19:14:39 -0400

    3/19 subs

--- ch06/exercises/substitutions.py
+++ ch06/exercises/substitutions.py
@@ -1,16 +1,20 @@
 import json
 def main():
     fptr = open("exercises/news.txt","r")
-    fptr.read()
-    file_pointer = open("exercises/subs.json","r")
-    new_news = json.loads(file_pointer)
-    print(new_news, type(new_news))
-    fptr = open("exercises/subs.json","w")
-    for k,v in new_news:
-        fptr.write(file_pointer[k] == file_pointer(v)) 
-    
+    file_text = fptr.read()
     fptr.close()
+
+    file_pointer = open("exercises/subs.json","r")
+    new_news = json.load(file_pointer)
+    fptr = open("exercises/subs.json","r")
+    for k,v in new_news.items():
+        file_text = file_text.replace(k,v) 
     file_pointer.close()
+
+    file_new = open("excersise/better_news.json", "r")
+    file_new.write(new_news)
+    file_new.close()
+
 
 
 main()
