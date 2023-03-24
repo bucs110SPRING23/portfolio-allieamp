a37cb054dbc9f01d7999f9236493cdb6b208f1f0
allieamp <aampel1@binghamton.edu>
2023-03-23 20:17:36 -0400

    cipher

--- ch06/exercises/cipher.py
+++ ch06/exercises/cipher.py
@@ -1,18 +1,17 @@
-import random
 def cipher(text):
     result = ""
     accumulator = 0
     shift = 19
     for char in text:
         accumulator += 1
+    set_shift = shift + accumulator
+    if set_shift % 2 == 0:
+        set_shift = int(set_shift/2)
+    else:
+        set_shift = int((set_shift+1)/2)
     for char in text:
         if char.isalpha():
             start = ord('A') if char.isupper() else ord('a')
-            set_shift = shift + accumulator
-            if set_shift % 2 == 0:
-                set_shift = int(set_shift/2)
-            else:
-                set_shift = int((set_shift+1)/2)
             new_pos = (ord(char) - start + set_shift) % 26
             char = chr(start + new_pos)
         result += char
