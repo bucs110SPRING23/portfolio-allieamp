f109a7ac1075883a241bfdead8f1b8ea2f6545c7
allieamp <aampel1@binghamton.edu>
2023-05-01 13:32:52 -0400

    "final exam work"

---
+++ ch10/final/main.py
@@ -0,0 +1,20 @@
+import hpapi
+import dapi
+
+def main():
+    hp = hpapi.HPAPI()
+    hplist = []
+    hplist.extend(hp.parse_json(hp.get()))
+    disney = dapi.DAPI()
+    dlist = []
+    data = disney.get(1)
+    for i in range(1,disney.get_pages(data)+1):
+        dlist.extend(disney.parse_json(disney.get(i)))
+    print("There are",len(hplist),"characters in the Harry Potter Films")
+    print("There are",len(dlist),"characters in Disney Films")
+
+main()
+
+#Questions
+##Is it concerningthat it takes a while?
+##Can you give me a sense of if I need more for uniqueness?
